225623e4cfaf7e32dc2bafeae17e642a168cdc5e
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-05-28 12:23:20 -0500

    bug fixes

--- build_controllers.sh
+++ build_controllers.sh
@@ -10,7 +10,7 @@ g++ -c -o $CONTROLLER_DIR/c0/controller_main.o $CONTROLLER_DIR/c0/controller_mai
 g++ -o $CONTROLLER_DIR/tmp0 $CONTROLLER_DIR/c0/controller.o $CONTROLLER_DIR/c0/controller_main.o
 gcc -c -o $CONTROLLER_DIR/c1/controller.o $CONTROLLER_DIR/c1/controller.c
 g++ -c -o $CONTROLLER_DIR/c1/controller_main.o $CONTROLLER_DIR/c1/controller_main.cpp
-g++ -o $CONTROLLER_DIR/tmp1 $CONTROLLER_DIR/c0/controller.o $CONTROLLER_DIR/c1/controller_main.o
+g++ -o $CONTROLLER_DIR/tmp1 $CONTROLLER_DIR/c1/controller.o $CONTROLLER_DIR/c1/controller_main.o
 gcc -c -o $CONTROLLER_DIR/c2/controller.o $CONTROLLER_DIR/c2/controller.c
 g++ -c -o $CONTROLLER_DIR/c2/controller_main.o $CONTROLLER_DIR/c2/controller_main.cpp
 g++ -o $CONTROLLER_DIR/tmp2 $CONTROLLER_DIR/c2/controller.o $CONTROLLER_DIR/c2/controller_main.o
