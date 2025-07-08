692b0dc18ec5bb8cf01684697f2b13601d2af95c
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-07-08 12:07:31 -0500

    add a random newline to maybe repair

--- vulns/vuln_stack_bof/controller.c
+++ vulns/vuln_stack_bof/controller.c
@@ -59,6 +59,7 @@ void interpolate_trajectory_point(
         if (processing_buffer[0] != 0) {
             double adjustment = processing_buffer[0] * 0.001;
             point_interp->positions[0] += adjustment;
+            
         }
         
     }
