4b17073c10a4b2e504716bf44927714f4fbeaf51
Cai Lemieux Mack <cailemieuxmack@gmail.com>
2025-07-09 19:54:41 -0500

    add a return 0;

--- vulns/bug_floating_point/controller.c
+++ vulns/bug_floating_point/controller.c
@@ -19,39 +19,41 @@ void interpolate_point(
     const MappedJointTrajectoryPoint point_1,
     const MappedJointTrajectoryPoint point_2,
     MappedJointTrajectoryPoint * point_interp, double delta)
+{
+  for (size_t i = 0; i < point_1.positions_length; i++)
   {
-    for (size_t i = 0; i < point_1.positions_length; i++)
-    {
-      point_interp->positions[i] = delta * point_2.positions[i] + (1.0 - delta) * point_1.positions[i];
-    }
-    for (size_t i = 0; i < point_1.positions_length; i++)
-    {
-      point_interp->velocities[i] =
-        delta * point_2.velocities[i] + (1.0 - delta) * point_1.velocities[i];
-
-        if ((int)point_2.velocities[i] % (int)point_1.velocities[i] != 0.0f) {
-          printf("%s", "point velocities not equal");
-        }
-
-    }
+    point_interp->positions[i] = delta * point_2.positions[i] + (1.0 - delta) * point_1.positions[i];
 
   }
-  
-  void interpolate_trajectory_point(
-    const MappedJointTrajectory traj_msg, const uint32_t cur_time_seconds,
-    MappedJointTrajectoryPoint * point_interp)
+  for (size_t i = 0; i < point_1.positions_length; i++)
   {
-    int traj_len = (int) traj_msg.points_length;
-    //auto last_time = traj_msg.points[traj_len - 1].time_from_start;
-    double total_time = traj_msg.points[traj_len - 1].time_from_start_sec + traj_msg.points[traj_len - 1].time_from_start_nsec * 1E-9;
-    //double total_time = last_time.sec + last_time.nanosec * 1E-9;
-  
-    size_t ind = cur_time_seconds * (traj_len / total_time);
-    ind = MIN( (double) ind, traj_len - 2);
-    double delta = cur_time_seconds - ind * (total_time / traj_len);
-    interpolate_point(traj_msg.points[ind], traj_msg.points[ind + 1], point_interp, delta);
-  
+    point_interp->velocities[i] =
+      delta * point_2.velocities[i] + (1.0 - delta) * point_1.velocities[i];
+
+      if ((int)point_2.velocities[i] % (int)point_1.velocities[i] != 0.0f) {
+        printf("%s", "point velocities not equal");
+
+      }
+
   }
+
+}
+  
+void interpolate_trajectory_point(
+  const MappedJointTrajectory traj_msg, const uint32_t cur_time_seconds,
+  MappedJointTrajectoryPoint * point_interp)
+{
+  int traj_len = (int) traj_msg.points_length;
+  //auto last_time = traj_msg.points[traj_len - 1].time_from_start;
+  double total_time = traj_msg.points[traj_len - 1].time_from_start_sec + traj_msg.points[traj_len - 1].time_from_start_nsec * 1E-9;
+  //double total_time = last_time.sec + last_time.nanosec * 1E-9;
+
+  size_t ind = cur_time_seconds * (traj_len / total_time);
+  ind = MIN( (double) ind, traj_len - 2);
+  double delta = cur_time_seconds - ind * (total_time / traj_len);
+  interpolate_point(traj_msg.points[ind], traj_msg.points[ind + 1], point_interp, delta);
+
+}
 
 
 int init() {
@@ -75,5 +77,6 @@ int step() {
     printf("Did we vote? %f\n", point_interp->positions[0]);
 
     out->vote = *point_interp;
+    return 0;
 
 }
