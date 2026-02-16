c23e607e84daffe773b77819652b929c0d669e5f
cailemieuxmack <cailemieuxmack@gmail.com>
2026-02-16 15:16:55 -0600

    uhhh

--- repair.sh
+++ repair.sh
@@ -11,5 +11,9 @@ cp src/example_7/controller/c$bad_num/controller.c repair/controller.c
 
 python3 make_testcases.py $1 $max_idx
 
+echo "before the repair"
+
 # call the llm repair tool
 python3 ~/Research/git/repair_tool/repair.py --test_dir repair/test/n1 --src_file repair/controller.c --header_file repair/controller.h --output_dir repair/repair_output
+
+echo "after the repair"
