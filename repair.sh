cac2d3c1c99d31f831e45a79b1aa71c00cf821bc
cailemieuxmack <cailemieuxmack@gmail.com>
2026-02-16 16:47:19 -0600

    hjlskhflfljshkjdsfshdlkfdljsdf

--- repair.sh
+++ repair.sh
@@ -18,7 +18,7 @@ echo "before the repair"
 # FIXME tool invocation is still wrong???
 # call the llm repair tool
 export PYTHONPATH="$HOME/Research/git/claude_repair:$PYTHONPATH"
-python3 -m apr_tool.main --test-dir $here_dir/repair/test/n1 --source $here_dir/repair/controller.c --header ~/Research/git/claude_repair/test_examples/controller.h --driver ~/Research/git/claude_repair/test_examples/test_driver.cpp --output $here_dir/repair/repair_output
+python3 -m apr_tool.main --test-dir $here_dir/repair/test/ --source $here_dir/repair/controller.c --header ~/Research/git/claude_repair/test_examples/controller.h --driver ~/Research/git/claude_repair/test_examples/test_driver.cpp --output $here_dir/repair/repair_output
 
 echo "after the repair"
 
