344d6bd8435a756ddd27c32c4d1365ad5534e7aa
cailemieuxmack <cailemieuxmack@gmail.com>
2026-02-16 15:29:17 -0600

    ahhhhhhhhhhh

--- repair.sh
+++ repair.sh
@@ -14,6 +14,6 @@ python3 make_testcases.py $1 $max_idx
 echo "before the repair"
 
 # call the llm repair tool
-python3 ~/Research/git/claude_repair/apr_tool/repair.py --test_dir repair/test/n1 --src_file repair/controller.c --header_file repair/controller.h --output_dir repair/repair_output
+python3 ~/Research/git/claude_repair/apr_tool/main.py --test_dir repair/test/n1 --src_file repair/controller.c --header_file repair/controller.h --output_dir repair/repair_output
 
 echo "after the repair"
