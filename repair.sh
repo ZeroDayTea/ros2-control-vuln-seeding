abb0478d785b6ebf2916bfd78077488f2d6edd6a
cailemieuxmack <cailemieuxmack@gmail.com>
2026-02-16 16:42:24 -0600

    hjlskhflf

--- repair.sh
+++ repair.sh
@@ -13,9 +13,12 @@ python3 make_testcases.py $1 $max_idx
 
 echo "before the repair"
 
+
+
+# FIXME tool invocation is still wrong???
 # call the llm repair tool
 export PYTHONPATH="$HOME/Research/git/claude_repair:$PYTHONPATH"
-python3 -m apr_tool.main --test_dir repair/test/n1 --src_file repair/controller.c --header_file repair/controller.h --output_dir repair/repair_output
+python3 -m apr_tool.main --test_dir $here_dir/repair/test/n1 --source $here_dir/repair/controller.c --header $here_dir/repair/controller.h --driver ~/Research/git/claude_repair/test_examples/test_driver.cpp --output $here_dir/repair/repair_output
 
 echo "after the repair"
 
