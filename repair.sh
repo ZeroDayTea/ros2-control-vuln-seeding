fb1653c730365013809727f3833ed3f7c9af44fb
cailemieuxmack <cailemieuxmack@gmail.com>
2026-02-23 09:11:01 -0600

    enable asan for repairs

--- repair.sh
+++ repair.sh
@@ -17,8 +17,9 @@ echo "before the repair"
 
 # FIXME tool invocation is still wrong???
 # call the llm repair tool
+# I think it is fixed.... maybe come back to this. Check the asan thing  --- I had forgotten to enable it...
 export PYTHONPATH="$HOME/Research/git/claude_repair:$PYTHONPATH"
-python3 -m apr_tool.main --test-dir $here_dir/repair/test/ --source $here_dir/repair/controller.c --header ~/Research/git/claude_repair/test_examples/controller.h --driver ~/Research/git/claude_repair/test_examples/test_driver.cpp --output $here_dir/repair/repair_output
+python3 -m apr_tool.main --enable-asan --test-dir $here_dir/repair/test/ --source $here_dir/repair/controller.c --header ~/Research/git/claude_repair/test_examples/controller.h --driver ~/Research/git/claude_repair/test_examples/test_driver.cpp --output $here_dir/repair/repair_output
 
 echo "after the repair"
 
